ecf610fd723982056c3d306b6b518a8b9aeab33b
sunmin Kim <miniks130@naver.com>
2022-04-13 23:24:17 +0900

    4/13

--- collector.py
+++ collector.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     # mysql db ip (자신의 PC에 DB를 구축 했을 경우 별도 수정 필요 없음)
     db_ip = 'localhost'  # localhost : 자신의 컴퓨터를 의미
     # mysql db 패스워드
-    db_passwd = '1234!@#$'
+    db_passwd = 'tjsals6092'
     # db port가 3306이 아닌 다른 port를 사용 하시는 분은 아래 변수에 포트에 맞게 수정하셔야 합니다.
     db_port = '3306'
 
